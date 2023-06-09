8eb6e1b2af42e1fc6ecdd97fae846619b205c452
JodexIndustries <0984134q@gmail.com>
2023-06-09 17:18:43 +0300

    1.0

--- src/main/java/com/jodexindustries/donatecase/api/Case.java
+++ src/main/java/com/jodexindustries/donatecase/api/Case.java
@@ -34,4 +34,66 @@ public class Case {
     public static int getKeys(String name, String player) {
         return getKeys(name, player);
     }
+
+    /**
+     * Is there a case on these coordinates?
+     * @param loc <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Location.html#toString()">Location#toString()</a>
+     * @return true/false
+     */
+    public static boolean hasCaseByLocation(String loc) {
+        return hasCaseByLocation(loc);
+    }
+
+    /**
+     * Get case type by location
+     * @param loc <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Location.html#toString()">Location#toString()</a>
+     * @return case type
+     */
+    public static String getCaseTypeByLocation(String loc) {
+        return getCaseTypeByLocation(loc);
+    }
+
+    /**
+     * Get case name by location
+     * @param loc <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Location.html#toString()">Location#toString()</a>
+     * @return case name
+     */
+    public static String getCaseNameByLocation(String loc) {
+        return getCaseNameByLocation(loc);
+    }
+
+    /**
+     * Is there a case with a name?
+     * @param name case name
+     * @return true/false
+     */
+    public static boolean hasCaseByName(String name) {
+        return hasCaseByName(name);
+    }
+    /**
+     * Are there cases that have been created?
+     * @param name case name
+     * @return true/false
+     */
+    public static boolean hasCaseDataByName(String name) {
+        return hasCaseDataByName(name);
+    }
+
+    /**
+     * Are there cases with a specific title?
+     * @param title case title
+     * @return true/false
+     */
+    public static boolean hasCaseByTitle(String title) {
+        return hasCaseByTitle(title);
+    }
+
+    /**
+     * Get a case name with a title
+     * @param title case title
+     * @return case name
+     */
+    public static String getCaseByTitle(String title) {
+        return getCaseByTitle(title);
+    }
 }
