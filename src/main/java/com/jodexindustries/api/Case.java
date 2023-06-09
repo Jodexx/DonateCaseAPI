a3a30c5097b87e93fd5befb8cf5caad56cde2b73
JodexIndustries <0984134q@gmail.com>
2023-06-09 11:11:53 +0300

    1.0

---
+++ src/main/java/com/jodexindustries/api/Case.java
@@ -0,0 +1,13 @@
+package com.jodexindustries.api;
+
+public class Case {
+    public static void setKeys(String casename, String player, int keys) {}
+
+    public static void addKeys(String casename, String player, int keys) {}
+
+    public static void removeKeys(String casename, String player, int keys) {}
+
+    public static int getKeys(String name, String player) {
+        return getKeys(name, player);
+    }
+}
