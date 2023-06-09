79e1e2bea5d7bea721cf7532f7c382dda1960d67
JodexIndustries <0984134q@gmail.com>
2023-06-09 16:01:21 +0300

    1.0

--- src/main/java/com/jodexindustries/donatecase/api/Case.java
+++ src/main/java/com/jodexindustries/donatecase/api/Case.java
@@ -1,12 +1,36 @@
 package com.jodexindustries.donatecase.api;
 
 public class Case {
+    /**
+     * Set case keys to a specific player
+     * @param casename case name
+     * @param player player name
+     * @param keys number of keys
+     */
     public static void setKeys(String casename, String player, int keys) {}
 
+    /**
+     * Add case keys to a specific player
+     * @param casename case name
+     * @param player player name
+     * @param keys number of keys
+     */
     public static void addKeys(String casename, String player, int keys) {}
 
+    /**
+     * Delete case keys for a specific player
+     * @param casename case name
+     * @param player player name
+     * @param keys number of keys
+     */
     public static void removeKeys(String casename, String player, int keys) {}
 
+    /**
+     * Get the keys to a certain player's case
+     * @param name case name
+     * @param player player name
+     * @return number of keys
+     */
     public static int getKeys(String name, String player) {
         return getKeys(name, player);
     }
