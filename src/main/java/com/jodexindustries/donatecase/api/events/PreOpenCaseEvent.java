3e5c211bbdde58fe92ef2563fdcab7bd678bcdc1
JodexIndustries <0984134q@gmail.com>
2023-06-12 13:45:08 +0300

    1.0.3

---
+++ src/main/java/com/jodexindustries/donatecase/api/events/PreOpenCaseEvent.java
@@ -0,0 +1,44 @@
+package com.jodexindustries.donatecase.api.events;
+
+import com.jodexindustries.donatecase.api.Case;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class PreOpenCaseEvent extends PlayerEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    protected boolean cancel;
+    String caseType;
+
+    public PreOpenCaseEvent(@NotNull final Player who, @NotNull final String caseType) {
+        super(who);
+        this.caseType = caseType;
+        cancel = false;
+    }
+    @NotNull
+    public String getCaseType() {
+        return new Case().getCaseByTitle(caseType);
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+}
