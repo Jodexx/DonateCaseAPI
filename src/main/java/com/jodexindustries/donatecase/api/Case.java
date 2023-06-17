b24782c620191f181b76b7c3eae52dc48979ca6f
JodexIndustries <0984134q@gmail.com>
2023-06-17 17:11:34 +0300

    1.0.4

--- src/main/java/com/jodexindustries/donatecase/api/Case.java
+++ src/main/java/com/jodexindustries/donatecase/api/Case.java
@@ -1,10 +1,44 @@
 package com.jodexindustries.donatecase.api;
 
+import com.jodexindustries.donatecase.dc.Main;
+import com.jodexindustries.donatecase.tools.Tools;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.jodexindustries.donatecase.dc.Main.customConfig;
+
 public class Case {
+    /**
+     * List of entities currently in use
+     */
+    public static List<ArmorStand> listAR = new ArrayList<>();
+    /**
+     * Open cases (active)
+     */
+    public static HashMap<Player, Location> openCase = new HashMap<>();
+    /**
+     * Active cases
+     */
+    public static HashMap<Location, String> ActiveCase = new HashMap<>();
+
+    /**
+     * Players, who opened cases (started scrolling)
+     */
+    public static List<Player> caseOpen = new ArrayList<>();
+
+
+    public static void saveLocation(String name, String type, Location lv) {}
+
     /**
      * Set case keys to a specific player
      * @param casename Case name
@@ -109,6 +143,17 @@ public class Case {
      */
 
     public static void startAnimation(Player player, Location location, String casename) {}
+
+    public static String getRandomGroup(String c) {
+        return getRandomGroup(c);
+    }
+
+    public static String getWinGroupId(String c, String winGroup) {
+        return getWinGroupId(c, winGroup);
+    }
+    public static String getWinGroupDisplayName(String c, String winGroup) {return getWinGroupDisplayName(c, winGroup);}
+
+    public static void onCaseOpenFinish(String casename, Player player, boolean needsound, String winGroup) {}
 
     /**
      * @param blockLocation Location
