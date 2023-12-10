39886ab4e3e56e24295185231d7a9d35e929623f
Jodexx <0984134q@gmail.com>
2023-12-10 17:12:31 +0200

    1.1.9

--- src/main/java/com/jodexindustries/donatecase/api/SubCommand.java
+++ src/main/java/com/jodexindustries/donatecase/api/SubCommand.java
@@ -17,15 +17,33 @@ public interface SubCommand {
     void execute(CommandSender sender, String[] args);
 
     /**
-     * Tab completions
-     * @param sender Source of the command
-     * @param args Passed command arguments
-     * @return Tab completions
+     * Get command tab completions
+     * @param sender Command sender
+     * @param args Command args
+     * @return tab completions
      */
     List<String> getTabCompletions(CommandSender sender, String[] args);
+
     /**
-     * Command type (ADMIN, MODER, PLAYER)
-     * @return SubCommandType
+     * Get command type
+     * @return command type
      */
     SubCommandType getType();
+
+    /**
+     * Get command arguments     *  to appear in the Command Description in /dc help
+     * @return Command arguments
+     */
+    default String[] getArgs() {
+        return null;
+    }
+
+    /**
+     * Get command description
+     * A description that should act as a description of the command in the /dc help
+     * @return Command description
+     */
+    default String getDescription() {
+        return null;
+    }
 }
